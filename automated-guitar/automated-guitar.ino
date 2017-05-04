a76d621186c35557acf5e86a97b3040c898f9709
Dan Lofaro <dan@danlofaro.com>
2017-05-04 16:05:14 -0400

    monofonic working

--- automated-guitar/automated-guitar.ino
+++ automated-guitar/automated-guitar.ino
@@ -1,54 +1,38 @@
-//Taimur Sohrab, Kishn Parbadia 10/20/2016   -- Worked on making note C (0x40) bring servo between two ranges
-                                           //-- also implemented flushing the notes to prevent key presses
-                                           //-- from backing up the servo
 
-#include <Servo.h>
+int nE3   = A0;
+int nF3   = A1;
+int nFS3  = A2;
+int nG3   = A3;
+int nA4   = A4;
+int nBb4  = A5;
 
 
-Servo myservo0;  // create servo object to control a servo
-Servo myservo1;  // create servo object to control a servo
-Servo myservo2;  // create servo object to control a servo
-Servo myservo3;  // create servo object to control a servo
-Servo myservo4;  // create servo object to control a servo
-Servo myservo5;  // create servo object to control a servo
-
-int low0 = 80; // desired edge of servo (0 - 180)
-int high0 = 70; // other desired edge of servo (0 - 180)
-int low1 = 84; // desired edge of servo (0 - 180)
-int high1 = 74; // other desired edge of servo (0 - 180)
-int low2 = 90; // desired edge of servo (0 - 180)
-int high2 = 80; // other desired edge of servo (0 - 180)
-int low3 = 84; // desired edge of servo (0 - 180)
-int high3 = 74; // other desired edge of servo (0 - 180)
-int low4 = 90; // desired edge of servo (0 - 180)
-int high4 = 80; // other desired edge of servo (0 - 180)
-int low5 = 86; // desired edge of servo (0 - 180)
-int high5 = 76; // other desired edge of servo (0 - 180)
   
 
 void setup() {
   //  Set MIDI baud rate:
-  Serial1.begin(31250);
-  Serial.begin(115200);
+  Serial.begin(31250);
+  //Serial.begin(115200);
+  pinMode(nE3, OUTPUT);
+  pinMode(nF3, OUTPUT);
+  pinMode(nFS3,OUTPUT);
+  pinMode(nG3, OUTPUT);
+  pinMode(nA4, OUTPUT);
+  pinMode(nBb4,OUTPUT);
 
-  // Servo Setup
-  myservo0.attach(6);  // attaches the servo on pin 9 to the servo object
-  myservo0.write(low0); //initial value for servo angle
+  digitalWrite(nE3,LOW);
+  digitalWrite(nF3,LOW);
+  digitalWrite(nFS3,LOW);
+  digitalWrite(nG3,LOW);
+  digitalWrite(nA4,LOW);
+  digitalWrite(nBb4,LOW);
+}
 
-  myservo1.attach(5);  // attaches the servo on pin 9 to the servo object
-  myservo1.write(low1); //initial value for servo angle
-
-  myservo2.attach(3);  // attaches the servo on pin 9 to the servo object
-  myservo2.write(low2); //initial value for servo angle
-
-  myservo3.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo3.write(low3); //initial value for servo angle
-
-  myservo4.attach(10);  // attaches the servo on pin 9 to the servo object
-  myservo4.write(low4); //initial value for servo angle
-
-  myservo5.attach(11);  // attaches the servo on pin 9 to the servo object
-  myservo5.write(low5); //initial value for servo angle
+int doHit(int note)
+{
+  digitalWrite(note,HIGH);
+  delay(50);
+  digitalWrite(note,LOW);
 }
 
 void loop() {
@@ -71,27 +55,27 @@ void loop() {
   int servoDelay = 50; //desired delay between servo rotation
 
   
-  if (Serial1.available() > 0) 
+  if (Serial.available() > 0) 
   {
      int velos = 0;
      int FLAG_NOTE_PRESSED = 0;
 
      
-     while(Serial1.available() > 0)
+     while(Serial.available() > 0)
      {
       int FLAG_NO_NOTE = 1;
 
       while(1 == FLAG_NO_NOTE)        /// No note flag
       {
-        while(Serial1.available() > 0)  /// Keep reading
+        while(Serial.available() > 0)  /// Keep reading
         {
-          note = Serial1.read();
+          note = Serial.read();
           if(((byte)note >= 0x90) & ((byte)note <= 0x9F))
           {
-            while(Serial1.available() == 0);
-            byte note2 = Serial1.read();
-            while(Serial1.available() == 0);
-            byte velos2 = Serial1.read();
+            while(Serial.available() == 0);
+            byte note2 = Serial.read();
+            while(Serial.available() == 0);
+            byte velos2 = Serial.read();
             FLAG_NOTE_PRESSED = 1;
             //Serial.println(note2, HEX);
             FLAG_NO_NOTE = 0;
@@ -100,56 +84,15 @@ void loop() {
             int val = map(note2*4, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
             //myservo.write(val);                  // sets the servo position according to the scaled value
             
-            if (note2 == 0x40){  // C flat played
-                myservo0.write(low0);
-                delay(servoDelay);
-                myservo0.write(high0);
-                delay(servoDelay);
-                myservo0.write(low0);    
-            }
-
-            else if (note2 == 0x41){  // C flat played
-                myservo1.write(low1);
-                delay(servoDelay);
-                myservo1.write(high1);
-                delay(servoDelay);
-                myservo1.write(low1);    
-            }
-            else if (note2 == 0x42){  // C flat played
-                myservo2.write(low2);
-                delay(servoDelay);
-                myservo2.write(high2);
-                delay(servoDelay);
-                myservo2.write(low2);    
-            }
-
-
-            else if (note2 == 0x43){  // C flat played
-                myservo3.write(low3);
-                delay(servoDelay);
-                myservo3.write(high3);
-                delay(servoDelay);
-                myservo3.write(low3);    
-            }
-
-            else if (note2 == 0x44){  // C flat played
-                myservo4.write(low4);
-                delay(servoDelay);
-                myservo4.write(high4);
-                delay(servoDelay);
-                myservo4.write(low4);    
-            }
-
-            else if (note2 == 0x45){  // C flat played
-                myservo5.write(low5);
-                delay(servoDelay);
-                myservo5.write(high5);
-                delay(servoDelay);
-                myservo5.write(low5);    
-            }
+            if (note2 == 0x40) doHit(nE3);
+            else if (note2 == 0x41) doHit(nF3);
+            else if (note2 == 0x42) doHit(nFS3);
+            else if (note2 == 0x43) doHit(nG3);
+            else if (note2 == 0x44) doHit(nA4);
+            else if (note2 == 0x45) doHit(nBb4);
             
-            while(Serial1.available()){  // Flush all remaining notes in queue 
-              Serial1.read();
+            while(Serial.available()){  // Flush all remaining notes in queue 
+              Serial.read();
             }
             
             break;
@@ -163,17 +106,11 @@ void loop() {
 }
 //  plays a MIDI note.  Doesn't check to see that
 //  cmd is greater than 127, or that data values are  less than 127:
-void noteOn2(int cmd, int pitch, int velocity) {
+void noteOn(int cmd, int pitch, int velocity) {
   Serial.print(cmd);
   Serial.print(pitch);
   Serial.print(velocity);
 }
 
 
-//  plays a MIDI note.  Doesn't check to see that
-//  cmd is greater than 127, or that data values are  less than 127:
-void noteOn(int cmd, int pitch, int velocity) {
-  Serial1.print(cmd);
-  Serial1.print(pitch);
-  Serial1.print(velocity);
-}
+
